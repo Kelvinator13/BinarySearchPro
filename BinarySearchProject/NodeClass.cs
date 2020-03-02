afd23df6ba3154c8b032d2035b2b5fe5e6d98ec1
kelvin omoregie <kayo2k14@gmail.com>
2020-03-02 09:51:21 -0600

    init

---
+++ BinarySearchProject/NodeClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchProject
+{
+    class NodeClass
+    {
+        public void TreeClass()
+        {
+             public class Node
+        {
+            public int data;
+            public Node Left;
+            public Node Right;
+            public void DisplayNode()
+            {
+                Console.WriteLine(data + " ");
+            }
+      
+        }
+        public Node root;
+        public NodeClass()
+        {
+            root = null;
+        }
+        public void Insert (int i)
+        {
+            Node newNode = new Node();
+            newNode.data = i;
+            if (root == null)
+                root = newNode;
+            else
+        }
+    }
+}
